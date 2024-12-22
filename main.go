7105c663173bfa87b2528639f5f36c7a559bdfa2
radu1902 <radu.pultea@gmail.com>
2024-12-23 01:48:00 +0200

    login ui

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+var users []User = []User{{"ionut", "qwer", []byte("salut")}, {"mircea", "asdf", []byte("buna")}}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Body)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "login.html")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	randVar := rand.Uint32() % 3
+	json.NewEncoder(w).Encode(randVar)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	usr := users[1]
+	fmt.Println(usr)
+	json.NewEncoder(w).Encode(usr)
+}
+
+func main() {
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/user", getUser)
+
+	http.ListenAndServe(":8080", nil)
+}
